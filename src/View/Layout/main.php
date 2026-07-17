2c67c77dea1e9cb7dcd94d66f088ac498c8f2327
Johan <144804317+Johan-Tnt@users.noreply.github.com>
2026-07-17 16:47:03 +0200

    Integrate exchange books page layout.

--- src/View/Layout/main.php
+++ src/View/Layout/main.php
@@ -11,12 +11,18 @@
     <link rel="stylesheet" href="assets/css/components/footer.css">
     <link rel="stylesheet" href="assets/css/pages/home.css">
     <link rel="stylesheet" href="assets/css/components/button.css">
+    <link rel="stylesheet" href="assets/css/components/book-card.css">
+    <link rel="stylesheet" href="assets/css/pages/books.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,400;0,600;1,400&family=Playfair+Display:wght@400&display=swap"
     rel="stylesheet">
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    
 </head>
 
 <body>
--- src/View/books.php
+++ src/View/books.php
@@ -1,8 +1,211 @@
-<!-- NOS LIVRES A L'ECHANGE -->
-<section>
-    <div>
+<!-- OUR BOOKS FOR EXCHANGE AND WANTED -->
+<section class="books_and_search">
+    <div class="container books_header">
 
         <h1>Nos Livres à l'échange</h1>
 
+        <form class="search_form" action="#" method="get">
+
+            <div class="search_input">
+
+                <i class="fa-solid fa-magnifying-glass  search_icon"></i>
+
+                <input
+                    class="search_field"
+                    type="search"
+                    name="search"
+                    placeholder="Rechercher un livre">
+            
+            </div>
+
+        </form>
+
     </div>
+
+</section>
+
+<!-- LIST OF BOOKS -->
+<section class="books_list">
+
+    <div class="container home_container books_container">
+
+        <article class="book_card exchange_books">
+            <img src="assets/images/pictures-books/esther.png" 
+            alt="Couverture du livre Esther">
+
+            <h3>Esther</h3>
+
+            <p class="written--secondary" >Alabaster</p>
+
+            <p class="written--secondary">Vendu par : CamilleClubLit</p>
+        </article>
+
+        <article class="book_card exchange_books">
+            <img src="assets/images/pictures-books/the-kingfolk-table.png" 
+            alt="Couverture du livre The Kingfolk Table">
+
+            <h3>The Kingfolk Table</h3>
+
+            <p class="written--secondary">Nathan Williams</p>
+
+            <p class="written--secondary">Vendu par : Natalire</p>
+        </article>
+
+        <article class="book_card exchange_books">
+            <img src="assets/images/pictures-books/wabi-sabi.png" 
+            alt="Couverture du livre Wabi Sabi">
+            
+            <h3>Wabi Sabi</h3>
+
+            <p class="written--secondary">Beth Kempton</p>
+
+            <p class="written--secondary">Vendu par : Alexlecture</p>
+        </article>
+
+        <article class="book_card exchange_books">
+            <img src="assets/images/pictures-books/milk-and-honey.png" 
+            alt="Couverture du livre Milk & Honey">
+            
+            <h3>Milk & Honey</h3>
+
+            <p class="written--secondary">Rupi Kaur</p>
+
+            <p class="written--secondary">Vendu par : Hugo1990_12</p>
+        </article>
+
+        <article class="book_card exchange_books">
+            <img src="assets/images/pictures-books/delight.png" 
+            alt="Couverture du livre Delight">
+            
+            <h3>Delight</h3>
+
+            <p class="written--secondary">Justin Rossow</p>
+
+            <p class="written--secondary">Vendu par : Juju1432</p>
+        </article>
+
+        
+        <article class="book_card exchange_books">
+            <img src="assets/images/pictures-books/milwaukee-mission.png" 
+            alt="Couverture du livre Milwaukee Mission">
+            
+            <h3>Milwaukee Mission</h3>
+
+            <p class="written--secondary">Elder Cooper Low</p>
+
+            <p class="written--secondary">Vendu par : Christiane75014</p>
+        </article>
+
+        <article class="book_card exchange_books">
+            <img src="assets/images/pictures-books/minimalist-graphics.png" 
+            alt="Couverture du livre Minimalist Graphics">
+            
+            <h3>Minimalist Graphics</h3>
+
+            <p class="written--secondary">Julia Schonlau</p>
+
+            <p class="written--secondary">Vendu par : Hamzalecture</p>
+        </article>
+
+        <article class="book_card exchange_books">
+            <img src="assets/images/pictures-books/hygge.png" 
+            alt="Couverture du livre Hygge">
+            
+            <h3>Hygge</h3>
+
+            <p class="written--secondary">Meik Wiking</p>
+
+            <p class="written--secondary">Vendu par : Hugo1990_12</p>
+        </article>
+        
+        <article class="book_card exchange_books">
+            <img src="assets/images/pictures-books/innovation.png" 
+            alt="Couverture du livre Innovation">
+            
+            <h3>Innovation</h3>
+
+            <p class="written--secondary">Matt Ridley</p>
+
+            <p class="written--secondary">Vendu par : Lou&Ben50</p>
+        </article>
+
+        <article class="book_card exchange_books">
+            <img src="assets/images/pictures-books/psalms.png" 
+            alt="Couverture du livre Psalms">
+            
+            <h3>Psalms</h3>
+
+            <p class="written--secondary">Alabaster</p>
+
+            <p class="written--secondary">Vendu par : Lolobzh</p>
+        </article>
+
+        <article class="book_card exchange_books">
+            <img src="assets/images/pictures-books/thinking-fast-slow.png" 
+            alt="Couverture du livre Thinking, Fast & Slow">
+            
+            <h3>Thinking, Fast & Slow</h3>
+
+            <p class="written--secondary">Daniel Kahneman</p>
+
+            <p class="written--secondary">Vendu par : Sas634</p>
+        </article>
+
+        <article class="book_card exchange_books">
+            <img src="assets/images/pictures-books/a-book-full-of-hope.png" 
+            alt="Couverture du livre A Book Full Of Hope">
+            
+            <h3>A Book Full Of Hope</h3>
+
+            <p class="written--secondary">Rupi Kaur</p>
+
+            <p class="written--secondary">Vendu par : ML95</p>
+        </article>
+
+        <article class="book_card exchange_books">
+            <img src="assets/images/pictures-books/the-subtle-art-of-not-giving-a-f.png" 
+            alt="Couverture du livre The Subtle Art Of...">
+            
+            <h3>The Subtle Art Of...</h3>
+
+            <p class="written--secondary">Mark Manson</p>
+
+            <p class="written--secondary">Vendu par : Verogo33</p>
+        </article>
+
+        <article class="book_card exchange_books">
+            <img src="assets/images/pictures-books/narnia.png" 
+            alt="Couverture du livre Narnia">
+            
+            <h3>Narnia</h3>
+
+            <p class="written--secondary">C.S Lewis</p>
+
+            <p class="written--secondary">Vendu par : AnikkaBrahms</p>
+        </article>
+
+        <article class="book_card exchange_books">
+            <img src="assets/images/pictures-books/company-of-one.png" 
+            alt="Couverture du livre Company Of One">
+            
+            <h3>Company Of One</h3>
+
+            <p class="written--secondary">Paul Jarvis</p>
+
+            <p class="written--secondary">Vendu par : Victoirefabr912</p>
+        </article>
+
+            <article class="book_card exchange_books">
+            <img src="assets/images/pictures-books/the-two-tower.png" 
+            alt="Couverture du livre The Two Tower">
+            
+            <h3>The Two Tower</h3>
+
+            <p class="written--secondary">J.R.R R Tolkien</p>
+
+            <p class="written--secondary">Vendu par : Lotrfanclub67</p>
+        </article>
+
+    </div>
+
 </section>
